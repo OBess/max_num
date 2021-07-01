a74588db7b8c0a9d7476ac69ac2d8686aaa8c246
OBess <dima.suschenko03@gmail.com>
2021-07-01 15:09:06 +0300

    Added main template functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,48 @@
 // C++
 #include <iostream>
+#include <cassert>
+
+using ull = unsigned long long;
+
+template <int N, ull num>
+constexpr ull max_int()
+{
+   if constexpr (N != 0)
+      return max_int<N - 1, num * 10 + 9>();
+   return num;
+}
+
+template <int N>
+constexpr ull max_int()
+{
+   if constexpr (N > 0 && N <= 19)
+      return max_int<N - 1, 9>();
+   return 0;
+}
 
 int main()
 {
-
+   static_assert(max_int<-1>() == 0);
+   static_assert(max_int<0>() == 0);
+   static_assert(max_int<1>() == 9);
+   static_assert(max_int<2>() == 99);
+   static_assert(max_int<3>() == 999);
+   static_assert(max_int<4>() == 9999);
+   static_assert(max_int<5>() == 99999);
+   static_assert(max_int<6>() == 999999);
+   static_assert(max_int<7>() == 9999999);
+   static_assert(max_int<8>() == 99999999);
+   static_assert(max_int<9>() == 999999999);
+   static_assert(max_int<10>() == 9999999999);
+   static_assert(max_int<11>() == 99999999999);
+   static_assert(max_int<12>() == 999999999999);
+   static_assert(max_int<13>() == 9999999999999);
+   static_assert(max_int<14>() == 99999999999999);
+   static_assert(max_int<15>() == 999999999999999);
+   static_assert(max_int<16>() == 9999999999999999);
+   static_assert(max_int<17>() == 99999999999999999);
+   static_assert(max_int<18>() == 999999999999999999);
+   static_assert(max_int<19>() == 9999999999999999999);
+   static_assert(max_int<20>() == 0);
    return EXIT_SUCCESS;
 }
